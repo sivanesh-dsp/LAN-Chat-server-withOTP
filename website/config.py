0a2afb19a01847132fc4f256537bd7aa074665be
Sivanesh B <60127773+sivanesh-dsp@users.noreply.github.com>
2022-05-17 11:48:29 +0530

    Update config.py

--- website/config.py
+++ website/config.py
@@ -5,5 +5,5 @@ class Config:
     # Load in enviornemnt variables
     TESTING = 'False'
     FLASK_DEBUG = 'True'
-    SECRET_KEY = 'youwontguessthiskey'
+    SECRET_KEY = 'youwontguessthiskeys'
     SERVER = socket.gethostbyname(socket.gethostname())
